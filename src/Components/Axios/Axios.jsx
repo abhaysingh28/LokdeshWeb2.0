c3bf4222ed3bad84076c57c1e0cebf549492eac0
अभय सिंह <99662885+abhaysingh28@users.noreply.github.com>
2022-12-22 16:41:02 +0530

    Add files via upload

---
+++ src/Components/Axios/Axios.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: "http://52.66.186.55/",
+  headers: {
+    "Content-Type": "application/json",
+  },    
+});
+
+export default Axios;
